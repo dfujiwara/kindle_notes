5a4ccb1f567bcf9f80774b5cbec59376ffaf1dce
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-12-12 23:19:39 -0800

    More work around parser

--- src/notesParser.ts
+++ src/notesParser.ts
@@ -1,11 +1,24 @@
-import { parse } from 'node-html-parser'
+import { parse, valid } from 'node-html-parser'
 import * as fs from 'fs'
 
-export const parseNotes = (path: string) => {
+type Notes = {
+    title: string,
+    notes: string[]
+}
+const bookTitleClassName = '.bookTitle'
+const noteTextClassName = '.noteText'
+
+export const parseNotes = (path: string): Notes => {
     if (!fs.existsSync(path)) {
-        throw new Error('error!')
+        throw new Error(`Error, the path ${path} doesn't exist`)
     }
     const pathContent = fs.readFileSync(path, 'utf8')
+    //if (!valid(pathContent)) {
+    //    throw new Error(`Error, the content in ${path} isn't considered valid`)
+    //}
     const root = parse(pathContent)
-    return root.toString()
+    const title = root.querySelector(bookTitleClassName)
+    const notes = root.querySelector(noteTextClassName)
+    console.log(notes)
+    return { title: title.rawText.trim(), notes: [notes.rawText.trim()]}
 }

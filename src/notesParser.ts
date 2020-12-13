e9e227832b4bbbd3c773ce40495c9ad4f6129dbf
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-12-13 14:32:22 -0800

    Use different html parsing library
    Given that the generated HTML is not well formatted, we have to do a bit
    more of manual processing to get the data in the right format.

--- src/notesParser.ts
+++ src/notesParser.ts
@@ -1,24 +1,86 @@
-import { parse, valid } from 'node-html-parser'
-import * as fs from 'fs'
+import * as fs from "fs";
+import { Parser } from "htmlparser2";
 
 type Notes = {
-    title: string,
-    notes: string[]
+  title: string;
+  notes: string[];
+};
+const bookTitleClassName = ".bookTitle";
+const noteTextClassName = ".noteText";
+
+enum Mode {
+    title,
+    notes,
+    others
 }
-const bookTitleClassName = '.bookTitle'
-const noteTextClassName = '.noteText'
+class NotesParser {
+  title: string = ""
+  notes: string[] = []
+  mode: Mode = Mode.others
+  currentBuffer: string = ""
+
+  onopentag(
+    name: string,
+    attribs: {
+      [s: string]: string;
+    }
+  ) {
+    this.processBuffer()
+    if (attribs["class"] == "bookTitle") {
+      this.mode = Mode.title
+      return
+    }
+    if (attribs["class"] == "noteText") {
+      this.mode = Mode.notes
+      return
+    }
+    this.mode = Mode.others
+  }
+
+  ontext(text: string) {
+    switch (this.mode) {
+        case Mode.title:
+            this.currentBuffer += text
+            break
+        case Mode.notes:
+            this.currentBuffer += text
+            break
+        case Mode.others:
+            break
+    }
+  }
+
+  onclosetag(name: string) {
+      this.processBuffer()
+      this.mode = Mode.others
+  }
+
+  private processBuffer() {
+      if (this.currentBuffer.length == 0) {
+        return
+      }
+      const trimmedBuffer = this.currentBuffer.trim()
+      switch (this.mode) {
+        case Mode.title:
+            this.title = trimmedBuffer
+            break
+        case Mode.notes:
+            this.notes.push(trimmedBuffer)
+            break
+        case Mode.others:
+            break
+      }
+      this.currentBuffer = ""
+  }
+}
 
 export const parseNotes = (path: string): Notes => {
-    if (!fs.existsSync(path)) {
-        throw new Error(`Error, the path ${path} doesn't exist`)
-    }
-    const pathContent = fs.readFileSync(path, 'utf8')
-    //if (!valid(pathContent)) {
-    //    throw new Error(`Error, the content in ${path} isn't considered valid`)
-    //}
-    const root = parse(pathContent)
-    const title = root.querySelector(bookTitleClassName)
-    const notes = root.querySelector(noteTextClassName)
-    console.log(notes)
-    return { title: title.rawText.trim(), notes: [notes.rawText.trim()]}
-}
+  if (!fs.existsSync(path)) {
+    throw new Error(`Error, the path ${path} doesn't exist`);
+  }
+  const pathContent = fs.readFileSync(path, "utf8");
+  const notesParser = new NotesParser();
+  const parser = new Parser(notesParser);
+  parser.parseComplete(pathContent);
+  return { title: notesParser.title, notes: notesParser.notes };
+};

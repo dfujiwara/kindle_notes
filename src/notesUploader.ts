3de1707d9bf6048ac1e320b33783527622bad7e8
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-12-26 22:48:03 -0800

    Initial uploader implementation

---
+++ src/notesUploader.ts
@@ -0,0 +1,14 @@
+import { Notes } from './notes'
+import fetch from 'node-fetch'
+
+const url = 'http://localhost:8080'
+
+export async function upload(notes: Notes): Promise<void> {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(notes),
+    })
+    const json = await response.json()
+    console.log(json)
+}

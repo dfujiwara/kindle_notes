45dd41417e5019769cbb1156f26a003d3c3c94cc
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2021-01-01 00:48:53 -0800

    More unit tests (#25)

--- src/httpHandlers.test.ts
+++ src/httpHandlers.test.ts
@@ -2,6 +2,7 @@ import { record, randomSelect } from './httpHandlers'
 import { Request, Response } from 'express'
 import { Notes } from './notes'
 import * as storage from './storage'
+import { mocked } from 'ts-jest/utils'
 
 jest.mock('./storage')
 
@@ -14,36 +15,30 @@ describe('Record HTTP endpoint', () => {
         } as Request
     }
 
-    test('should only support POST method', async () => {
-        const configuration: [string, number][] = [
-            ['GET', 405],
-            ['PUT', 405],
-            ['POST', 200],
-        ]
-        configuration.forEach(async ([method, status]) => {
-            let recordedStatus = 0
-            const response = {
-                sendStatus: (status: number) => {
-                    recordedStatus = status
-                },
-            } as Response
+    let recordedStatus = 0
+    let response: Response
+    beforeEach(() => {
+        recordedStatus = 0
+        response = {
+            sendStatus: (status: number) => {
+                recordedStatus = status
+            },
+        } as Response
+    })
+
+    describe.each([
+        ['GET', 405],
+        ['PUT', 405],
+        ['POST', 200],
+    ])('testing with %s', (method, status) => {
+        test(`expects status code of ${status}`, async () => {
             const request = requestGenerator(method, 'application/json')
             await record(request, response)
             expect(recordedStatus).toBe(status)
         })
     })
 
-    describe('content type', () => {
-        let recordedStatus = 0
-        let response: Response
-        beforeEach(() => {
-            recordedStatus = 0
-            response = {
-                sendStatus: (status: number) => {
-                    recordedStatus = status
-                },
-            } as Response
-        })
+    describe('when checking content-type', () => {
         test('should reject content type that is not application/json', async () => {
             const request = requestGenerator('POST', 'text/html')
             await record(request, response)
@@ -55,28 +50,50 @@ describe('Record HTTP endpoint', () => {
             expect(recordedStatus).toBe(200)
         })
     })
+
+    test('should handle exception when recording notes', async () => {
+        const mockedStorage = mocked(storage)
+        mockedStorage.record.mockImplementation(() => {
+            throw new Error('Mocked error')
+        })
+        const request = requestGenerator('POST', 'application/json')
+        await record(request, response)
+        expect(recordedStatus).toBe(500)
+    })
 })
 
 describe('RandomSelect HTTP endpoint', () => {
-    test('should only support GET method', async () => {
-        const configuration: [string, number][] = [
-            ['GET', 200],
-            ['PUT', 405],
-            ['POST', 405],
-        ]
-        configuration.forEach(async ([method, status]) => {
-            let recordedStatus = 0
-            const response = {
-                sendStatus: (status: number) => {
-                    recordedStatus = status
-                },
-                send: () => {
-                    recordedStatus = 200
-                },
-            } as Response
+    let recordedStatus = 0
+    let response: Response
+    beforeEach(() => {
+        response = {
+            sendStatus: (status: number) => {
+                recordedStatus = status
+            },
+            send: () => {
+                recordedStatus = 200
+            },
+        } as Response
+    })
+    describe.each([
+        ['GET', 200],
+        ['PUT', 405],
+        ['POST', 405],
+    ])('testing with %s', (method, status) => {
+        test(`expects status code of ${status}`, async () => {
             const request = { method } as Request
             await randomSelect(request, response)
             expect(recordedStatus).toBe(status)
         })
+    })
+    test('should handle exception when randomly selecting notes', async () => {
+        const mockedStorage = mocked(storage)
+        mockedStorage.randomSelect.mockImplementation(() => {
+            throw new Error('Mocked error')
+        })
+        const request = { method: 'GET' } as Request
+        await randomSelect(request, response)
+        expect(recordedStatus).toBe(500)
+        expect(mockedStorage.randomSelect.mock.calls.length).toBe(1)
     })
 })
